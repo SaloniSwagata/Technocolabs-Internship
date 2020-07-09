9e3f1f1261c29ff4b976343d9ccb457483bfc00c
Saloni <saloniswagata@gmail.com>
2020-07-10 00:19:04 +0530

    Added code to generate Line Graph

--- Projects/Minor Project/WeatherApp.py
+++ Projects/Minor Project/WeatherApp.py
@@ -10,7 +10,7 @@ root.geometry("400x200")
 
 
 
-def grapher():
+def bargraph():
     owm = pyowm.OWM('d90703eea69f90f613b88279fea113db')
     degree_sign= u'\N{DEGREE SIGN}'
     mgr = owm.weather_manager()
@@ -55,8 +55,8 @@ def grapher():
     ax.set_xticklabels(xpos)
 
     # inserting y axis label
-    ax.set_yticks(np.arange(0, 110, 10))
-    ax.set_yticklabels(np.arange(0, 110, 10))
+    ax.set_yticks(np.arange(0, 100, 10))
+    ax.set_yticklabels(np.arange(0, 100, 10))
     plt.xticks(xpos,time)
     plt.title(f'Weather Forecast For {place}')
     plt.ylabel(f"Temperature ({degree_sign}C)")
@@ -86,7 +86,69 @@ def grapher():
     #canvas.get_tk_widget().grid(row=3,column=0)
 
 
+def linegraph():
+    owm = pyowm.OWM('d90703eea69f90f613b88279fea113db')
+    degree_sign= u'\N{DEGREE SIGN}'
+    mgr = owm.weather_manager()
+    place = e.get()
+    forecaster = mgr.forecast_at_place(place, '3h')
+    forecast = forecaster.forecast
+    weather_list = forecast.weathers
+    temp_min=[]
+    temp_max=[]
+    time=[]
+    date=0
+    t_min=[]
+    t_max=[]
+    for weather in weather_list:
+        d=(weather.reference_time('iso').split(" "))[0]
+        if d!=date:
+            date=d
+            if t_min!=[]:
+                total1=min(t_min)
+                total2=max(t_max)
+                temp_min.append(total1)
+                temp_max.append(total2)
+                time.append(d.split("-")[2]+"/"+d.split("-")[1])
+            t_min=[]
+            t_max=[]
+            t_min.append(int(weather.temperature(unit='celsius')['temp_min']))
+            t_max.append(int(weather.temperature(unit='celsius')['temp_max']))
+        else:
+            t_min.append(int(weather.temperature(unit='celsius')['temp_min']))
+            t_max.append(int(weather.temperature(unit='celsius')['temp_max']))
 
+    xpos = np.arange(len(time))    
+    plt.plot(xpos, temp_min , color='blue', label="Minimum Temperature")
+    plt.plot(xpos, temp_max, color='orange', label="Maximum Temperature")
+    plt.title(f'Weather Forecast For {place}')
+    plt.ylabel(f"Temperature ({degree_sign}C)")
+    plt.xlabel("Day")
+    plt.xticks(xpos,time)
+    plt.yticks(np.arange(0, 100, 10))
+
+    # zip joins x and y coordinates in pairs
+    for x,y in zip(xpos,temp_min):
+        label = "{}".format(y)
+        plt.annotate(label, # this is the text
+                     (x,y), # this is the point to label
+                     textcoords="offset points", # how to position the text
+                     xytext=(0,10), # distance from text to points (x,y)
+                     ha='center') # horizontal alignment can be left, right or center
+
+    # zip joins x and y coordinates in pairs
+    for x,y in zip(xpos,temp_max):
+        label = "{}".format(y)
+        plt.annotate(label, # this is the text
+                     (x,y), # this is the point to label
+                     textcoords="offset points", # how to position the text
+                     xytext=(0,10), # distance from text to points (x,y)
+                     ha='center') # horizontal alignment can be left, right or center
+
+
+    
+    plt.legend()
+    canvas = plt.show()
 
 Title = Label(root, text="Welcome to the Weather App!\n")
 #Title.pack()
@@ -100,8 +162,10 @@ Title.grid(row=0,column=0)
 text.grid(row=1,column=0)
 e.grid(row=1,column=2)
 
-myButton = Button(root, text="Get Weather Forecast", command= grapher, padx=10, pady=5)
+myButton = Button(root, text="Get Weather Forecast in Bar Graph", command= bargraph, padx=10, pady=5)
+myButton1 = Button(root, text="Get Weather Forecast in Line Graph", command= linegraph, padx=10, pady=5)
 myButton.grid(row=3,column=2)
+myButton1.grid(row=6,column=2)
 
 
 
